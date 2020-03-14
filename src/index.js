a0e86221e9c2490a0747b3ef911d317316e07790
Uladzimir Biarnatski <vvbernacky@gmail.com>
2020-03-14 22:38:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const brackets = bracketsConfig.map(b => b.join(""));
+  let index = 0;
+  while (index !== brackets.length) {
+      if (str.includes(brackets[index])) {
+          str = str.replace(brackets[index], "");
+          index = 0;
+          continue;
+      }
+      index += 1;
+  }
+  return !Boolean(str);
+};
